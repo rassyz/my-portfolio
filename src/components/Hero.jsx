643a6666fba4598d0450917a0ce89e52cee9e169
RasyidAbdulRauf <rasyidabdulrauf05@gmail.com>
2026-09-25 11:39:00 +0700

    chore: replace portfolio CV with latest version

--- src/components/Hero.jsx
+++ src/components/Hero.jsx
@@ -79,7 +79,7 @@ const Hero = () => {
 
           <div className="flex items-center gap-3">
             <ButtonPrimary
-              href="/docs/cv-data-qa-rasyid.pdf"
+              href="/docs/cv-qa-data-rasyid.pdf"
               label="Download CV"
               icon="download"
               download={true}
